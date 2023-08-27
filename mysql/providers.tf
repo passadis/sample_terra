beca4fed6f4b9a1a7750b70f0418e950ddfea63e
K.Passadis <passadis@gmail.com>
2023-08-27 23:43:37 +0300

    Update providers.tf

--- mysql/providers.tf
+++ mysql/providers.tf
@@ -1,1 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.70.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # version = "=3.39.1"
+  features {}
+  subscription_id = var.azure_subid
+  tenant_id       = var.azure_tenantid
+  client_id       = var.azure_appid
+  client_secret   = var.azure_spsecret
+}
 
