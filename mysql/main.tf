1d6c9a00251f009aa26f3339a2adfedb7188f5d7
K.Passadis <passadis@gmail.com>
2023-08-27 23:41:56 +0300

    Update main.tf

--- mysql/main.tf
+++ mysql/main.tf
@@ -1,1 +1,35 @@
+resource "azurerm_mysql_server" "example" {
+  name                = "db-mysqlserver"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  administrator_login          = "mysqluser"
+  administrator_login_password = "1q2w3e4r!!"
+
+  sku_name   = "B_Gen5_2"
+  storage_mb = 5120
+  version    = "5.7"
+
+  auto_grow_enabled                 = true
+  backup_retention_days             = 7
+  geo_redundant_backup_enabled      = false
+  public_network_access_enabled     = true
+  ssl_enforcement_enabled           = true
+  ssl_minimal_tls_version_enforced  = "TLS1_2"
+}
+
+resource "azurerm_mysql_database" "db" {
+  name                = "mydb"
+  resource_group_name = azurerm_resource_group.example.name
+  server_name         = azurerm_mysql_server.example.name
+  charset             = "utf8"
+  collation           = "utf8_unicode_ci"
+}
+
+resource "azurerm_mysql_configuration" "example" {
+  name                = "interactive_timeout"
+  resource_group_name = azurerm_resource_group.example.name
+  server_name         = azurerm_mysql_server.example.name
+  value               = "600"
+}
 
