5a97c0202bbefb14839fb3b9e71f28f5d4f43efa
K.Passadis <passadis@gmail.com>
2023-08-27 23:45:33 +0300

    Update variables.tf

--- mysql/variables.tf
+++ mysql/variables.tf
@@ -1,1 +1,20 @@
-
+# Azure SP Secret
+variable "azure_spsecret" {
+  type        = string
+  description = "Azure SP Secret"
+}
+# Azure APPid
+variable "azure_appid" {
+  type        = string
+  description = "Azure APPid"
+}
+# Azure Tenant ID
+variable "azure_tenantid" {
+  type        = string
+  description = "Azure APPid"
+}
+# Azure Subscription ID
+variable "azure_subid" {
+  type        = string
+  description = "Azure APPid"
+}
